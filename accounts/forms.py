e53ca2a1d9d120b6c8504ecf592af4d58680f628
Ripudaman Singh <ripudaman050203@gmail.com>
2022-07-07 16:20:14 +0530

    fifth commit

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+# from django import forms
+# from django.cntrib.auth.models import User
+# form django.contrib.auth.forms import UserCreationForm
+#
+# class RegistrationForm(UserCreationForm):
+#     class Meta:
+#         model= User
+#         fields = {'username' , 'first_name' , 'last_name', ''}
